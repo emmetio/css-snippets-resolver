8b9b828726e1bf04cdf44c1f773056edc4e0971b
Sergey Chikuyonok <serge.che@gmail.com>
2017-01-30 01:51:11 +0300

    Oops, fixed module builder entry point

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 export default {
-	entry: './lib/score.js',
+	entry: './index.js',
 	targets: [
 		{format: 'cjs', dest: 'dist/css-snippets-resolver.cjs.js'},
 		{format: 'es',  dest: 'dist/css-snippets-resolver.es.js'}
