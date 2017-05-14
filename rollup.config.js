bd90bd8bc6dbd05ba5e8dcb4e552d4548e82fe1d
Sergey Chikuyonok <serge.che@gmail.com>
2017-05-14 19:46:14 +0300

    Updated Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 export default {
 	entry: './index.js',
+	exports: 'named',
 	targets: [
 		{format: 'cjs', dest: 'dist/css-snippets-resolver.cjs.js'},
 		{format: 'es',  dest: 'dist/css-snippets-resolver.es.js'}
